f52b194524d9246288611403c5e4901bfddc6be9
WithStomach <czhang2004@163.com>
2024-05-19 17:50:48 +0800

    lv6

--- hello.c
+++ hello.c
@@ -1,14 +1,11 @@
 int main()
 {
-    int a = 0;
+    int a = 2;
     if (a)
     {
-        return 0;
+        a = a + 1;
     }
     else
-    {
-        return 1;
-    }
-
+        a = 0; // 在实际写 C/C++ 程序的时候别这样, 建议 if 的分支全部带大括号
     return a;
 }
